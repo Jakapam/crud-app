c447b244da283ea7022d5da2f284f09d3bc97257
github username <jstn.mnld@gmail.com>
2019-04-01 20:53:41 -0400

    feat: add adminuser creation

--- server/manage.py
+++ server/manage.py
@@ -1,16 +1,37 @@
 import os
-from flask_script import Manager
+import sys
+from flask_script import Manager,prompt, prompt_pass
 from flask_migrate import Migrate, MigrateCommand
 from config import DevelopmentConfig
 from app import app, db
-from models import User
+from models import User, Question, Token, Modifier, Response
 
 app.config.from_object(DevelopmentConfig)
 
 migrate = Migrate(app, db)
 manager = Manager(app)
-
 manager.add_command('db', MigrateCommand)
+
+@manager.command
+def createadminuser():
+    username = prompt('Username')
+    email = prompt('E-Mail')
+
+    password = prompt_pass('User password')
+    password_confirm = prompt_pass('Confirmed password')
+
+    if not password == password_confirm:
+        sys.exit('\nCould not create user: Passwords did not match')
+    user = User(username=username, email=email, is_admin=True)
+    user.set_password(password)
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        app.logging.error("Error creating user: {}".format(e))
+
+    app.logging.info("Admin '{}' created".format(username))
 
 if __name__ == '__main__':
     manager.run()
